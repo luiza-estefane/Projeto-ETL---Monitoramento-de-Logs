73433d744e888aca52cfade0ae451c9dfe51e40b
luiza-estefane <luizaluzsouza1@gmail.com>
2026-07-22 18:56:51 -0300

    Atualização  - Projeto ETL

--- gerador_logs.py
+++ gerador_logs.py
@@ -2,49 +2,43 @@ import psycopg2
 from datetime import datetime
 import random
 import os
-import time
 from dotenv import load_dotenv
 
-
-# segurança 
+# segurança
 
 load_dotenv()
-
 
 try:
     related = psycopg2.connect(
         host="localhost",
         port="5433",
         database="dw_logs",
-        user=os.getenv("POSTGRES_USER"), 
-        password=os.getenv("POSTGRES_PASSWORD") 
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
     )
+    
     cursor = related.cursor()
-    print("Conexão com o Postgres realizada com sucesso!\n")
+    print(" Conexão com o Postgres realizada com sucesso!\n")
 
-  
-    #  Geração de Logs Dinâmicos
- 
-    # Listas para simular o comportamento de um sistema 
+    
+    #  Geração de Logs aleatorios
+   
     niveis_severidade = ["INFO", "WARNING", "ERROR"]
     mensagens_sistema = [
         "Transação processada com sucesso.",
         "Timeout de conexão com o servidor externo.",
         "Falha de autenticação: credenciais inválidas.",
         "Serviço reiniciado automaticamente.",
-        "Pico de uso de CPU detectado.",
         "Permissão negada ao acessar o diretório base.",
         "Backup diário concluído."
     ]
 
-    # sorteia automaticamente
+    # Sorteia automaticamente
     nivel = random.choice(niveis_severidade)
     mensagem = random.choice(mensagens_sistema)
     timestamp = datetime.now()
 
- 
- 
-    # Ajuste o nome da tabela (staging_logs) e colunas conforme o seu banco criado no Docker
+    
     query_insercao = """
         INSERT INTO staging_logs (data_hora, nivel_alerta, mensagem_log)
         VALUES (%s, %s, %s);
@@ -54,16 +48,15 @@ try:
     cursor.execute(query_insercao, (timestamp, nivel, mensagem))
     related.commit()
     
-    print(f"Log inserido com sucesso: [{nivel}] {mensagem}")
+    print(f" Log inserido com sucesso: [{nivel}] {mensagem}")
 
 except Exception as erro:
     print(f" Ocorreu um erro no pipeline: {erro}")
 
 finally:
-   
-    # 5. conclusão
   
+    #  Conclusão 
     if 'related' in locals() and related:
         cursor.close()
         related.close()
-        print("\nConexão com o banco encerrada com segurança.")
+        print("\n Conexão com o banco encerrada com segurança.")
